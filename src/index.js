5e2543010b78d3b1d3b3ba1a63837a56e7b53d0d
edanyn <edwardanyn@gmail.com>
2023-02-16 11:25:40 +0300

    init: done with test

--- src/index.js
+++ src/index.js
@@ -38,9 +38,51 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+       
+function string_to_morse(expr) {
+    let r = "";
+    for (i of expr) {
+        if (i == " ") {
+            r += "*,";
+        } else {
+        r += Object.fromEntries(Object.entries(MORSE_TABLE).map(([key,value])=>[value,key]))[i] + ",";
+        }
+    }
+    return r.substring(0, r.length - 1);
+} 
+
+function morse_to_bin(morse) {
+    let r = "";
+    for (i of morse) {
+        if (i == ".") {
+            r += "10";
+        } else if (i == "-") {
+            r += "11";
+        } else if (i == ",") {
+            r += ",";
+        } else {
+            r += "*";
+        }
+    }
+    return r.split(",");
+}
+
+function pad_bin(bin) {
+let r = "";
+for (i of bin) {
+    if (i == "*") {
+        r += "*".repeat(10);
+    } else {
+        r += "0".repeat(10 - i.length) + i;
+    }
+}
+return r;
+}
+    
+return pad_bin(morse_to_bin(string_to_morse(expr)));
 }
 
 module.exports = {
     decode
 }
+console.log(decode('call the police'));
